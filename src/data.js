d9d6fb3c0eca0ba044d1e8c55a7faa4c246bf37d
camila <camiramirezpp@gmail.com>
2018-06-07 10:26:34 -0400

    Estructura html básica

---
+++ src/data.js
@@ -0,0 +1,15 @@
+window.computeUsersStats = (users, progress, courses) => {
+
+};
+
+window.sortUsers = (users, orderBy, orderDirection) => {
+
+};
+
+window.filterUsers = (user, search) => {
+
+};
+
+window.processCohortData = (options) => {
+
+};
