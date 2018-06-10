aa7ef4e869ef6634dcbce5f37325de531cf920ba
Vanessa Azócar <v.azocar.adasme@gmail.com>
2018-06-10 17:57:57 -0300

    otros cabios css

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 /* funcion de Ingresar contraseña */
-function go() {
-  if (document.form.password.value === "laboratoria" && document.form.login.value === "Valentina") { 
+function entrar() {
+  if (document.form.email.value === 'valentina@laboratoria.cl' && document.form.password.value === 'laboratoria') { 
     document.form.submit(); 
   } else { 
-    alert("Por favor ingrese, nombre de usuario y contraseña correctos."); 
+    alert('¡Datos inválidos! <br> Por favor ingrese nombre de usuario y contraseña correctos.</br>'); 
   } 
 } 
   
