c7ae8867ae2b03eb3f55442571e6f4f48f0ce84b
Vanessa Azócar <v.azocar.adasme@gmail.com>
2018-06-11 12:05:02 -0300

    centrando logo

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 /* funcion de Ingresar contraseña */
 function entrar() {
-  if (document.form.email.value === 'valentina@laboratoria.cl' && document.form.password.value === 'laboratoria') { 
-    document.form.submit(); 
-  } else { 
-    alert('¡Datos inválidos! <br> Por favor ingrese nombre de usuario y contraseña correctos.</br>'); 
-  } 
-} 
-  
+  if (document.form.email.value === 'valentina@laboratoria.cl' && document.form.password.value === 'laboratoria') {
+    document.form.submit();
+  } else {
+    alert('¡Datos inválidos! <br> Por favor ingrese nombre de usuario y contraseña correctos.</br>');
+  }
+}
