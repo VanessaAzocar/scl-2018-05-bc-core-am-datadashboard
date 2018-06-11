9c929463602bcccf8843022d97792884bb3f175d
camila <camiramirezpp@gmail.com>
2018-06-11 12:13:05 -0400

    commit previo a synk

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ function entrar() {
   if (document.form.email.value === 'valentina@laboratoria.cl' && document.form.password.value === 'laboratoria') { 
     document.form.submit(); 
   } else { 
-    alert('¡Datos inválidos! <br> Por favor ingrese nombre de usuario y contraseña correctos.</br>'); 
+    alert('¡Datos inválidos! <br>Por favor ingrese nombre de usuario y contraseña correctos.</br>'); 
   } 
 } 
   
