48d24a3eb4485f006d3c1a878a63c9f5e826aabc
Vanessa Azócar <v.azocar.adasme@gmail.com>
2018-06-11 12:39:24 -0300

    arreglando nombres

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ function entrar() {
   if (document.form.email.value === "valentina@laboratoria.cl" && document.form.password.value === "laboratoria") {
     document.form.submit();
   } else {
-    alert("¡Datos inválidos!" + "<br> Por favor ingrese nombre de usuario y contraseña correctos.</br>");
-    }
+    alert("¡Datos inválidos! Por favor ingrese nombre de usuario y contraseña correctos.");
+  }
 }
