c576b015e53abf862eb72875cc99f2b4649f5182
Vanessa Azócar <v.azocar.adasme@gmail.com>
2018-06-13 13:51:59 -0300

    cambios contraseña

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 
 /* funcion de Ingresar contraseña */
 function entrar() {
-  if (document.form.email.value === "valentina@laboratoria.cl" && document.form.password.value === "laboratoria") {
-    const btn_hide = document.getElementById('buttonlogin');
-    btn_hide.addEventListener("click", () => {
-      document.getElementsByClassName("ingresar")[0].classList.add("buttonlogin");
-      document.getElementsByClassName("afterEntering")[0].classList.remove("afterEntering");
-    })
+  if (document.form.email.value === 'valentina@laboratoria.cl' && document.form.password.value === 'laboratoria') {
+    const btnhide = document.getElementById('buttonlogin');
+    btnhide.addEventListener('click', () => {
+      document.getElementsByClassName('ingresar')[0].classList.add('buttonlogin');
+      document.getElementsByClassName('afterEntering')[0].classList.remove('afterEntering');
+    }
+    );
   } else {
-    alert("¡Datos inválidos! Por favor ingrese nombre de usuario y contraseña correctos.");
+    document.getElementById('email').innerHTML = '¡Dato inválido!';
+    document.getElementById('password').innerHTML = '¡Dato inválido!';
   }
 }
-
