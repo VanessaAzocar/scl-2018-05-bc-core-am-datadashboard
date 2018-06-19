2477ffd0dee7cc50e13468afcc2b88015548de97
Vanessa Azócar <v.azocar.adasme@gmail.com>
2018-06-19 14:10:03 -0300

    textos ok en ingles

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 
 /* funcion de Ingresar contraseña */
-function entrar() {
+function enter() {
   if (document.form.email.value === 'valentina@laboratoria.cl' && document.form.password.value === 'laboratoria') {
-    const btnhide = document.getElementById('buttonlogin');
+    const btnhide = document.getElementById('buttonLogin');
     btnhide.addEventListener('click', () => {
-      document.getElementsByClassName('ingresar')[0].classList.add('buttonlogin');
+      document.getElementsByClassName('ingresar')[0].classList.add('buttonLogin');
       document.getElementsByClassName('afterEntering')[0].classList.remove('afterEntering');
     }
     );
@@ -13,29 +13,29 @@ function entrar() {
   }
 }
 
-//Este boton es el que abre el listado de pre admision en el menú superior//
-const btnPre = document.getElementById('buttomPre');
+/* Este boton es el que abre el listado de pre admision en el menú superior*/
+const btnPre = document.getElementById('btnPreadmission');
 btnPre.addEventListener('click', () => {
-  document.getElementsByClassName('listadoPreadmision')[0].classList.remove('listadoPreadmision');
+  document.getElementsByClassName('listPreadmission')[0].classList.remove('listPreadmission');
 });
 
-//Con esto aparece el listado de postulantes en un div vacío
-const btn = document.getElementById('buttomPre'); //para el proyecto tendriamos que usar otro, como getby..
-const container = document.getElementById('parrafoListado'); //este es el div vacio 
-const usersJSON = "../data/cohorts/lim-2018-03-pre-core-pw/users.json";
+/* Con esto aparece el listado de postulantes en un div vacío */
+const btn = document.getElementById('btnPreadmission'); /* para el proyecto tendriamos que usar otro, como getby..*/
+const container = document.getElementById('paragraphList'); /* este es el div vacio */
+const usersJSON = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
 
 fetch(usersJSON)
   .then(response => response.json())
   .then(data => {
     console.log(data);
     renderUsers(data);
-  })
+  });
 
-const renderUsers = (data) => { //recorre el objeto y me devuelve el nombre de cada una
+const renderUsers = (data) => { /* recorre el objeto y me devuelve el nombre de cada una */
   btn.addEventListener('click', () => {
     const render = data.forEach(element => {
-      return container.innerHTML += `<p>${element.name}</p>`
-    }) 
-    return render;    
-  })
-}
+      return container.innerHTML += `<p>${element.name}</p>`;
+    });
+    return render;
+  });
+};
