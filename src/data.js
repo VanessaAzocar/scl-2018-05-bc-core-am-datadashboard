11d3108daf96f88b0011ba33e5648728c9ab51e7
camila <camiramirezpp@gmail.com>
2018-06-20 12:32:02 -0400

    probando div vacio

--- src/data.js
+++ src/data.js
@@ -1,28 +1,5 @@
 /* Con esto aparece el listado de postulantes en un div vacío */
-/*window.onload = () => {
-  dataJSON ();
-}
-const btn = document.getElementById('btnPreadmission'); 
-const container = document.getElementById('paragraphList');  este es el div vacio 
-const usersJSON = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
 
-fetch(usersJSON)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    renderUsers(data);
-  });
-
-const renderUsers = (data) => { /* recorre el objeto y me devuelve el nombre de cada una 
-  btn.addEventListener('click', () => {
-    const render = data.forEach(element => {
-      return container.innerHTML += `<ul><li><a>${element.name}</a></li></ul>`;
-    });
-    return render;
-  });
-}; */
-
-//prueba2
 window.onload = () => {
   dataJSON ();
 }
@@ -37,19 +14,45 @@ function dataJSON () {
     console.log(data);
     renderUsers(data);
 })
+
 const renderUsers = data => {
   btnPre.addEventListener('click', () => {
     const render = data.forEach(element => {
       const contenedorData = document.createElement('div');
       const contenedor = document.getElementById('paragraphList');
       contenedor.appendChild(contenedorData);
-      let titulo_text= document.createTextNode(element.name);
-      contenedorData.appendChild(titulo_text);
+      let listName= document.createTextNode(element.name);
+      contenedorData.appendChild(listName);
     })
     return contenedorData;
   })
 }
 
 }
+
+
+
+
+
+/*
+const btnUser = document.getElementById('btnPreadmission'); 
+const container = document.getElementById('paragraphList');  //este es el div vacio 
+const usersJSON = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
+
+fetch(usersJSON)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    renderUsers(data);
+  });
+
+const renderUsers = (data) => {  recorre el objeto y me devuelve el nombre de cada una 
+  btn.addEventListener('click', () => {
+    const render = data.forEach(element => {
+      return container.innerHTML += `<ul><li><a>${element.name}</a></li></ul>`;
+    });
+    return render;
+  });
+}; */
 
 
